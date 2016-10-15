55b80006670384718df04d04c68b3f751c38fe31
Rohit <robeat101@gmail.com>
2016-10-15 13:37:06 -0700

    Publish and viz start and goal location

--- src/Astar.py
+++ src/Astar.py
@@ -6,13 +6,16 @@ Description: Read a map, get a start and goal position, and perform Astar search
             Publish list of searched cells and frontier cells. 
 """
 import rospy
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
-from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Point
+from nav_msgs.msg import OccupancyGrid, GridCells
 
 class Astar: 
 
     def beginSearch(self):
-        rospy.logwarn("Search has begun.")
+        self.updateStartLocation()
+        self.updateGoal()
+
+
 
     def defineMap(self, map): 
         self.isMapDefined = True
@@ -23,6 +26,31 @@ class Astar:
         self.isRobotLocationSet = True
         self.robotLocation = robotLocation
         return self.testSearchConditions()
+
+    def updateStartLocation(self): 
+        self.startLocationCells = self.newGridCell()
+        startLoc = Point(); 
+        startLoc.x = self.robotLocation.pose.pose.position.x
+        startLoc.y = self.robotLocation.pose.pose.position.y
+        startLoc.z = 0
+        self.startLocationCells.cells.append(startLoc)
+        self.pub_start.publish(self.startLocationCells)
+    
+    def updateGoal(self):
+        self.goalCells = self.newGridCell()
+        goalLoc = Point(); 
+        goalLoc.x = self.navGoal.pose.position.x
+        goalLoc.y = self.navGoal.pose.position.y
+        goalLoc.z = 0
+        self.goalCells.cells.append(goalLoc)
+        self.pub_goal.publish(self.goalCells)
+
+    def newGridCell(self): 
+        gridcell = GridCells()
+        gridcell.cell_height = self.robotResolution
+        gridcell.cell_width = self.robotResolution
+        gridcell.header.frame_id = 'map'
+        return gridcell
 
     def setNavGoal(self, navGoal): 
         self.isNavGoalSet = True
@@ -51,6 +79,12 @@ class Astar:
         self.sub_robotLocation = rospy.Subscriber('/initialpose', PoseWithCovarianceStamped, self.setRobotLocation, queue_size=1)
         self.sub_navGoal = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.setNavGoal, queue_size=1)
 
+        #Setup publishers
+        self.pub_start = rospy.Publisher('/robot/start', GridCells, latch=True, queue_size=1)
+        self.pub_goal = rospy.Publisher('/robot/goal', GridCells, latch=True, queue_size=1)
+
+
+        
 
 
 
