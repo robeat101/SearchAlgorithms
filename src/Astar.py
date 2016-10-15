f74c570ec828429a766f89b4df553924fb5fdcd0
Rohit <robeat101@gmail.com>
2016-10-15 01:48:38 -0700

    Fix import and subscriber type issues

--- src/Astar.py
+++ src/Astar.py
@@ -5,19 +5,35 @@ Author: Rohit Bansal
 Description: Read a map, get a start and goal position, and perform Astar search. 
             Publish list of searched cells and frontier cells. 
 """
-
-from geometry_msgs.msg import PoseWithCovarianceStamped
+import rospy
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import OccupancyGrid
 
 class Astar: 
 
     def beginSearch(self):
-        
-    def setRobotLocation(self): 
+        rospy.logwarn("Search has begun.")
 
-    def setNavGoal(self): 
+    def defineMap(self, map): 
+        self.isMapDefined = True
+        self.map = map
+        return self.testSearchConditions()
+        
+    def setRobotLocation(self, robotLocation): 
+        self.isRobotLocationSet = True
+        self.robotLocation = robotLocation
+        return self.testSearchConditions()
+
+    def setNavGoal(self, navGoal): 
+        self.isNavGoalSet = True
+        self.navGoal = navGoal
+        return self.testSearchConditions()
 
     def testSearchConditions(self): 
-        
+        if(self.isMapDefined == True and self.isNavGoalSet == True and self.isRobotLocationSet == True):
+            return self.beginSearch()
+        return False            
+
 
     def __init__(self, robotResolution = 0.2): 
         #Initialize node
@@ -31,9 +47,9 @@ class Astar:
 
 
         #Setup subscribers
-        self.map = rospy.Subscriber('/map', OccupancyGrid, self.beginSerarch, queue_size=1)
-        self.robotLocation = rospy.Subscriber('/initialpose', self.setRobotLocation, queue_size=1)
-        self.navGoal = rospy.Subscriber('/move_base_simple/goal', self.setNavGoal, queue_size=1)
+        self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.defineMap, queue_size=1)
+        self.sub_robotLocation = rospy.Subscriber('/initialpose', PoseWithCovarianceStamped, self.setRobotLocation, queue_size=1)
+        self.sub_navGoal = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.setNavGoal, queue_size=1)
 
 
 
